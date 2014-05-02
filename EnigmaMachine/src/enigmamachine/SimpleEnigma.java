6dd12930f1c548e1689a33e3d104a17f6c50de53
James Morris <jamesmorris2@gmail.com>
2014-05-02 13:17:20 -0400

    Added encode(), decode(), initializeRings(), and rotateRings() methods. The only working one is initializeRings(). encode() and decode() are partially implemented. rotateRings() not implemented at all.

--- EnigmaMachine/src/enigmamachine/SimpleEnigma.java
+++ EnigmaMachine/src/enigmamachine/SimpleEnigma.java
@@ -14,4 +14,61 @@ public class SimpleEnigma
     {
         
     }
+    
+    public String encode(String pPlainText)
+    {
+        String sPlainText = pPlainText.toUpperCase();
+        StringBuilder sbEncodedText = new StringBuilder();
+        
+        //Turn spaces into #
+        sPlainText = sPlainText.replace(' ', '#');
+        
+        for(int i = 0; i < sPlainText.length(); i++)
+        {
+            if(Character.isLetter(sPlainText.charAt(i)) || sPlainText.charAt(i) == '#')
+            {
+                sbEncodedText.append(c);
+            }
+            else
+            {
+                sbEncodedText.append(sPlainText.charAt(i));
+            }
+        }
+        
+        return sbEncodedText.toString();
+    }
+    
+    public String decode(String pEncodedText)
+    {
+        String sEncodedText = pEncodedText.toUpperCase();
+        StringBuilder sbPlainText = new StringBuilder();
+        
+        for(int i = 0; i < sEncodedText.length(); i++)
+        {
+            if(Character.isLetter(sEncodedText.charAt(i)) || sEncodedText.charAt(i) == '#')
+            {
+                sbPlainText.append(c);
+            }
+            else
+            {
+                sbPlainText.append(sEncodedText.charAt(i));
+            }
+        }
+        
+//        sbPlainText.re
+        
+        return sbPlainText.toString();
+    }
+    
+    void initializeRings()
+    {
+        OuterRotor = "#BDFHJLNPRTVXZACEGIKMOQSUWY";
+        MiddleRotor = "#EJOTYCHMRWAFKPUZDINSXBGLWV";
+        InnerRotor = "#GNUAHOVBIPWCJQXDKRYELSZFMT";
+    }
+    
+    void rotateRings()
+    {
+        
+    }
 }
