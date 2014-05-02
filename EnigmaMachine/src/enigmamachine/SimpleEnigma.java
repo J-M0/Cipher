00a40ae9c80a4809f05beedf4b5b07dd6920460a
James Morris <jamesmorris2@gmail.com>
2014-05-02 15:47:27 -0400

    Used Kyle's rotateKey() code from RotatingCipher.java to implement rotateRings(). Also added a main() method for testing. The message output from main() is gibberish. I still need to work on the decoding.

--- EnigmaMachine/src/enigmamachine/SimpleEnigma.java
+++ EnigmaMachine/src/enigmamachine/SimpleEnigma.java
@@ -43,7 +43,7 @@ public class SimpleEnigma
             if(Character.isLetter(sPlainText.charAt(i)) || sPlainText.charAt(i) == '#')
             {
                 sbEncodedText.append(OuterRotor.charAt(MiddleRotor.indexOf(OuterRotor.charAt(InnerRotor.indexOf(sPlainText.charAt(i))))));
-//                rotateRings();
+                rotateRings();
             }
             else
             {
@@ -65,7 +65,7 @@ public class SimpleEnigma
             if(Character.isLetter(sEncodedText.charAt(i)) || sEncodedText.charAt(i) == '#')
             {
                 sbPlainText.append(rotatedInner.charAt(OuterRotor.indexOf(rotatedMiddle.charAt(OuterRotor.indexOf(sEncodedText.charAt(i))))));
-//                rotateRings();
+                rotateRings();
             }
             else
             {
@@ -89,14 +89,52 @@ public class SimpleEnigma
     {
         if(MiddleRotorCount == 26)
         {
-            //rotate MiddleRotor            
+            //rotate MiddleRotor
+            //Code based on Kyle's rotateKey() code in RotatingCipher.java
+            StringBuilder changeKey = new StringBuilder(rotatedMiddle);
+            char firstChar = rotatedMiddle.charAt(0);
+            char lastChar = rotatedMiddle.charAt(25);
+            changeKey.setCharAt(25,firstChar);
+            for(int i = 0; i < 25; i++)
+            {
+                changeKey.setCharAt((24 - i), rotatedMiddle.charAt(25-i));
+            }
+            rotatedMiddle = changeKey.toString();
+            
             //rotate InnerRotor
+            changeKey = new StringBuilder(rotatedInner);
+            firstChar = rotatedInner.charAt(0);
+            lastChar = rotatedInner.charAt(25);
+            changeKey.setCharAt(25,firstChar);
+            for(int i = 0; i < 25; i++)
+            {
+                changeKey.setCharAt((24 - i), rotatedInner.charAt(25-i));
+            }
+            rotatedInner = changeKey.toString();
+            
             MiddleRotorCount = 0;
         }
         else
         {
             //rotate MiddleRotor
+            //Code based on Kyle's rotateKey() code in RotatingCipher.java
+            StringBuilder changeKey = new StringBuilder(rotatedMiddle);
+            char firstChar = rotatedMiddle.charAt(0);
+            char lastChar = rotatedMiddle.charAt(25);
+            changeKey.setCharAt(25,firstChar);
+            for(int i = 0; i < 25; i++)
+            {
+                changeKey.setCharAt((24 - i), rotatedMiddle.charAt(25-i));
+            }
+            rotatedMiddle = changeKey.toString();
             MiddleRotorCount++;
         }
+    }
+    
+    public static void main(String[] args)
+    {
+        SimpleEnigma enigma = new SimpleEnigma();
+        
+        System.out.println(enigma.decode("OKNNWRDHGERPILRLAMFZF#FMUC"));
     }
 }
