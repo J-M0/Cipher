cd8858544d5f7773d5492925cdbd9157f2a370dc
James Morris <jamesmorris2@gmail.com>
2014-05-02 12:35:45 -0400

    Initial commit of SimpleEnigma.java

---
+++ EnigmaMachine/src/enigmamachine/SimpleEnigma.java
@@ -0,0 +1,17 @@
+package enigmamachine;
+
+/**
+ *
+ * @author James Morris
+ */
+public class SimpleEnigma
+{
+    String OuterRotor = "#BDFHJLNPRTVXZACEGIKMOQSUWY";
+    String MiddleRotor = "#EJOTYCHMRWAFKPUZDINSXBGLWV";
+    String InnerRotor = "#GNUAHOVBIPWCJQXDKRYELSZFMT";
+    
+    SimpleEnigma()
+    {
+        
+    }
+}
